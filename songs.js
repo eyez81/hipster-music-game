8c94d7b06b150106ac4f48aacf34c40dfe6b10d3
eyez81 <139944014+eyez81@users.noreply.github.com>
2025-05-10 14:31:18 +0300

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,224 @@
+export default songsList;
+// רשימת 100 השירים המלאה
+const songsList = [
+  {
+    song: "Love Me Do",
+    artist: "The Beatles",
+    year: 1962,
+    link: "https://www.youtube.com/watch?v=0pGOFX1D_jg"
+  },
+  {
+    song: "I Want to Hold Your Hand",
+    artist: "The Beatles",
+    year: 1963,
+    link: "https://www.youtube.com/watch?v=jenWdylTtzs"
+  },
+  {
+    song: "Like a Rolling Stone",
+    artist: "Bob Dylan",
+    year: 1965,
+    link: "https://www.youtube.com/watch?v=IwOfCgkyEj0"
+  },
+  {
+    song: "Yesterday",
+    artist: "The Beatles",
+    year: 1965,
+    link: "https://www.youtube.com/watch?v=NrgmdOz227I"
+  },
+  {
+    song: "Respect",
+    artist: "Aretha Franklin",
+    year: 1967,
+    link: "https://www.youtube.com/watch?v=6FOUqQt3Kg0"
+  },
+  {
+    song: "Purple Haze",
+    artist: "Jimi Hendrix",
+    year: 1967,
+    link: "https://www.youtube.com/watch?v=WGoDaYjdfSg"
+  },
+  {
+    song: "Hey Jude",
+    artist: "The Beatles",
+    year: 1968,
+    link: "https://www.youtube.com/watch?v=A_MjCqQoLLA"
+  },
+  {
+    song: "Gimme Shelter",
+    artist: "The Rolling Stones",
+    year: 1969,
+    link: "https://www.youtube.com/watch?v=RbmS3tQJ7Os"
+  },
+  {
+    song: "Let It Be",
+    artist: "The Beatles",
+    year: 1970,
+    link: "https://www.youtube.com/watch?v=QDYfEBY9NM4"
+  },
+  {
+    song: "Imagine",
+    artist: "John Lennon",
+    year: 1971,
+    link: "https://www.youtube.com/watch?v=YkgkThdzX-8"
+  },
+  {
+    song: "Stairway to Heaven",
+    artist: "Led Zeppelin",
+    year: 1971,
+    link: "https://www.youtube.com/watch?v=QkF3oxziUI4"
+  },
+  {
+    song: "What's Going On",
+    artist: "Marvin Gaye",
+    year: 1971,
+    link: "https://www.youtube.com/watch?v=H-kA3UtBj4M"
+  },
+  {
+    song: "Superstition",
+    artist: "Stevie Wonder",
+    year: 1972,
+    link: "https://www.youtube.com/watch?v=0CFuCYNx-1g"
+  },
+  {
+    song: "Piano Man",
+    artist: "Billy Joel",
+    year: 1973,
+    link: "https://www.youtube.com/watch?v=gxEPV4kolz0"
+  },
+  {
+    song: "סע לאט",
+    artist: "אריק איינשטיין",
+    year: 1974,
+    link: "https://www.youtube.com/watch?v=ZwTbFd8kdII"
+  },
+  {
+    song: "No Woman, No Cry",
+    artist: "Bob Marley & The Wailers",
+    year: 1974,
+    link: "https://www.youtube.com/watch?v=pHlSE9j5FGY"
+  },
+  {
+    song: "Bohemian Rhapsody",
+    artist: "Queen",
+    year: 1975,
+    link: "https://www.youtube.com/watch?v=fJ9rUzIMcZQ"
+  },
+  {
+    song: "Dancing Queen",
+    artist: "ABBA",
+    year: 1976,
+    link: "https://www.youtube.com/watch?v=xFrGuyw1V8s"
+  },
+  {
+    song: "עטור מצחך",
+    artist: "אריק איינשטיין",
+    year: 1977,
+    link: "https://youtu.be/WZM4iXT3-ic?si=Di_ZaqtRCUH98c1M"
+  },
+  {
+    song: "Hotel California",
+    artist: "Eagles",
+    year: 1977,
+    link: "https://www.youtube.com/watch?v=BciS5krYL80"
+  },
+  {
+    song: "Don't Stop Me Now",
+    artist: "Queen",
+    year: 1978,
+    link: "https://www.youtube.com/watch?v=HgzGwKwLmgM"
+  },
+  {
+    song: "הללויה",
+    artist: "גלי עטרי וחלב ודבש",
+    year: 1979,
+    link: "https://www.youtube.com/watch?v=7N79pMN0Ni4"
+  },
+  {
+    song: "Another Brick in the Wall",
+    artist: "Pink Floyd",
+    year: 1979,
+    link: "https://www.youtube.com/watch?v=YR5ApYxkU-U"
+  },
+  {
+    song: "מדינה קטנה",
+    artist: "תיסלם",
+    year: 1981,
+    link: "https://www.youtube.com/watch?v=f8s3JxJl5f0"
+  },
+  {
+    song: "Don't Stop Believin'",
+    artist: "Journey",
+    year: 1981,
+    link: "https://www.youtube.com/watch?v=1k8craCGpgs"
+  },
+  {
+    song: "הפרח בגני",
+    artist: "זוהר ארגוב",
+    year: 1982,
+    link: "https://www.youtube.com/watch?v=SMkXTiPnwxk"
+  },
+  {
+    song: "Thriller",
+    artist: "Michael Jackson",
+    year: 1982,
+    link: "https://www.youtube.com/watch?v=sOnqjkJTMaA"
+  },
+  {
+    song: "תן לי יד",
+    artist: "שלום חנוך",
+    year: 1983,
+    link: "https://www.youtube.com/watch?v=bLu9VfOcj5M"
+  },
+  {
+    song: "Billie Jean",
+    artist: "Michael Jackson",
+    year: 1983,
+    link: "https://www.youtube.com/watch?v=Zi_XLOBDo_Y"
+  },
+  {
+    song: "Every Breath You Take",
+    artist: "The Police",
+    year: 1983,
+    link: "https://www.youtube.com/watch?v=OMOGaugKpzs"
+  },
+  {
+    song: "Sweet Dreams (Are Made of This)",
+    artist: "Eurythmics",
+    year: 1983,
+    link: "https://www.youtube.com/watch?v=qeMFqkcPYcg"
+  },
+  {
+    song: "Beat It",
+    artist: "Michael Jackson",
+    year: 1983,
+    link: "https://www.youtube.com/watch?v=oRdxUFDoQe0"
+  },
+  {
+    song: "חופשי זה לגמרי לבד",
+    artist: "בנזין",
+    year: 1984,
+    link: "https://www.youtube.com/watch?v=OBd6P88zK5M"
+  },
+  {
+    song: "Hallelujah",
+    artist: "Leonard Cohen",
+    year: 1984,
+    link: "https://www.youtube.com/watch?v=YrLk4vdY28Q"
+  },
+  {
+    song: "עוד לא תמו כל פלאייך",
+    artist: "ריטה",
+    year: 1986,
+    link: "https://www.youtube.com/watch?v=P1HUF8L5iYE"
+  },
+  {
+    song: "Livin' on a Prayer",
+    artist: "Bon Jovi",
+    year: 1986,
+    link: "https://www.youtube.com/watch?v=lDK9QqIzhwk"
+  },
+  {
+    song: "עוף גוזל",
+    artist: "אריק איינשטיין",
+    year: 1987,
+    link: "https://www.youtube.
